2af14978db0911d121b0d3dc0f4176da709c8de1
Laura <laura.gregorio@live.u-tad.com>
2022-11-22 22:17:46 +0100

    Funcionalidad tlf1 ok

--- src/vehiculo.java
+++ src/vehiculo.java
@@ -1,10 +1,13 @@
 
 public class vehiculo {
 
-	public static void main(String[] args) {
+	public static void arrancar() {
 		// TODO Auto-generated method stub
 		System.out.println("Brrrrr...acaba de arrancar");
 
+	}
+	public static void main(String[] args) {
+		arrancar();
 	}
 
 }
